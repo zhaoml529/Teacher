f09e5a8e55c9bd996ac00241196bb46a292e8da1
zhaoml529 <zhaoml529@163.com>
2014-12-27 03:46:42 +0800

    jpa

--- src/com/lyd/soft/entity/Message.java
+++ src/com/lyd/soft/entity/Message.java
@@ -4,15 +4,20 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * 站内信
  * @author LYD
  *
  */
+
+@Entity
+@Table(name = "T_MESSAGE")
 public class Message implements Serializable {
 
 	/**
