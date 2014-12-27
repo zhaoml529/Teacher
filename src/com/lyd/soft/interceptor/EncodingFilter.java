4350bc0c2f4eaf1532a61431a8536dce363c611d
zhaoml529 <zhaoml529@163.com>
2014-12-28 02:31:17 +0800

    编码过滤器

---
+++ src/com/lyd/soft/interceptor/EncodingFilter.java
@@ -0,0 +1,34 @@
+package com.lyd.soft.interceptor;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter{
+
+	private String encoding;
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding(this.encoding);
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
