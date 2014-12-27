c1322b8b787c2ca9343ddb07de995b8b0c9e3c08
zhaoml529 <zhaoml529@163.com>
2014-12-28 02:30:07 +0800

    分页有问题

--- src/com/lyd/soft/action/NewsAction.java
+++ src/com/lyd/soft/action/NewsAction.java
@@ -2,12 +2,17 @@ package com.lyd.soft.action;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.lyd.soft.entity.News;
+import com.lyd.soft.pagination.Pagination;
+import com.lyd.soft.pagination.PaginationThreadUtils;
 import com.lyd.soft.service.INewsService;
 
 @Controller
@@ -17,11 +22,16 @@ public class NewsAction {
 	@Autowired
 	private INewsService newsService;
 	
-	@RequestMapping( value = "/toList_page")
-	public String toList(Model model) throws Exception {
+	@RequestMapping("/toList_page")
+	public String toList(HttpServletRequest request, Model model) throws Exception {
+		String pageNum = request.getParameter("pageNum");
+		String testName = request.getParameter("newName");
+		System.out.println(pageNum+" ----------------- "+testName);
 		List<News> list = this.newsService.toList();
+		Pagination pagination = PaginationThreadUtils.get();
+		model.addAttribute("page", pagination.getPageStr());
 		model.addAttribute("newsList", list);
-		return null;
+		return "news/list_news";
 	}
 	
 }
