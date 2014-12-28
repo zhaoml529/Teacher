dbbb22abd6c50551f1044e01f45800a264ef1958
zhaoml529 <zhaoml529@163.com>
2014-12-29 00:30:20 +0800

    登录验证

---
+++ src/com/lyd/soft/interceptor/LoginInterceptor.java
@@ -0,0 +1,27 @@
+package com.lyd.soft.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.lyd.soft.entity.Teacher;
+import com.lyd.soft.util.BeanUtils;
+import com.lyd.soft.util.UserUtils;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		System.out.println(request.getServletPath()+" -------------------------");
+		Teacher user = UserUtils.getUserFromSession(session);
+		if(!BeanUtils.isBlank(user)){
+			return true;
+		}
+		response.sendRedirect(request.getContextPath() + "/loginAction/login_view");
+		return false;
+	}
+
+}
