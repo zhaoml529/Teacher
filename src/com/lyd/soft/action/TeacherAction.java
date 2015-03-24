7fb36e5580e512e3c4b878a29042179020c9ea4d
zhaoml529 <zhaoml529@163.com>
2015-03-24 22:47:06 +0800

    清理垃圾

--- src/com/lyd/soft/action/TeacherAction.java
+++ src/com/lyd/soft/action/TeacherAction.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
