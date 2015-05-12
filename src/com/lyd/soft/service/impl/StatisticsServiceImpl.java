2a32f3a5fcc7a5e87885558a9edd01098c658deb
zhaoml529 <zhaoml529@163.com>
2015-05-12 10:27:30 +0800

    统计条件

--- src/com/lyd/soft/service/impl/StatisticsServiceImpl.java
+++ src/com/lyd/soft/service/impl/StatisticsServiceImpl.java
@@ -21,7 +21,7 @@ public class StatisticsServiceImpl implements  IStatisticsService{
 	@Transactional(propagation=Propagation.NOT_SUPPORTED, readOnly=true)
 	public Integer getSubjectCountByYEAR(String type, String date)
 			throws Exception {
-		String sql = "select count(*) as teaching from t_subject where type=:type and is_delete=0 and date_format(create_date, '%Y')=:date";
+		String sql = "select count(*) as teaching from t_subject where type=:type and is_delete=0 and date_format(sub_date, '%Y')=:date";
 		Map<String, Object> paramMap = new HashMap<String, Object>();  
 	    paramMap.put("type", type);  
 	    paramMap.put("date", date);  
@@ -33,7 +33,7 @@ public class StatisticsServiceImpl implements  IStatisticsService{
 	@Transactional(propagation=Propagation.NOT_SUPPORTED, readOnly=true)
 	public Integer getPaperCountByYEAR(String type, String date)
 			throws Exception {
-		String sql = "select count(*) as teaching from t_paper where type=:type and is_delete=0 and date_format(create_date, '%Y')=:date";
+		String sql = "select count(*) as teaching from t_paper where type=:type and is_delete=0 and date_format(publish_date, '%Y')=:date";
 		Map<String, Object> paramMap = new HashMap<String, Object>();  
 	    paramMap.put("type", type);  
 	    paramMap.put("date", date);  
@@ -45,7 +45,7 @@ public class StatisticsServiceImpl implements  IStatisticsService{
 	@Transactional(propagation=Propagation.NOT_SUPPORTED, readOnly=true)
 	public Integer getBookCountByYEAR(String type, String date)
 			throws Exception {
-		String sql = "select count(*) as teaching from t_book where type=:type and is_delete=0 and date_format(create_date, '%Y')=:date";
+		String sql = "select count(*) as teaching from t_book where type=:type and is_delete=0 and date_format(publish_date, '%Y')=:date";
 		Map<String, Object> paramMap = new HashMap<String, Object>();  
 	    paramMap.put("type", type);  
 	    paramMap.put("date", date);  
@@ -57,7 +57,7 @@ public class StatisticsServiceImpl implements  IStatisticsService{
 	@Transactional(propagation=Propagation.NOT_SUPPORTED, readOnly=true)
 	public Integer getAwardCountByYEAR(String type, String date)
 			throws Exception {
-		String sql = "select count(*) as teaching from t_awarding where type=:type and is_delete=0 and date_format(create_date, '%Y')=:date";
+		String sql = "select count(*) as teaching from t_awarding where type=:type and is_delete=0 and date_format(achieve_date, '%Y')=:date";
 		Map<String, Object> paramMap = new HashMap<String, Object>();  
 	    paramMap.put("type", type);  
 	    paramMap.put("date", date);  
